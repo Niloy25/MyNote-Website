2142c220ce7c647c27712e4541b3032ae2d2ddc7
Niloy Mondal <niloykolkata59@gmail.com>
2021-10-25 14:50:01 +0530

    Final Commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            This is About Page.
+        <div className="container" style={{fontFamily: 'Zen Old Mincho, serif', fontSize: '30px'}}>
+            This is About Page. This is a simple react application to note your daily activities. I am not focus on the design here. I am only focus on coding like CRUD operation in this application.
         </div>
     )
 }
